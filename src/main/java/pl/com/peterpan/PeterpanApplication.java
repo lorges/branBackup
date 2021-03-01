d7d73b82ee7d306114441843ce9e5a3aa96c3358
Lorges <piotrekjakubiak1994@gmail.com>
2021-03-01 21:48:40 +0100

    Initial project commit

---
+++ src/main/java/pl/com/peterpan/PeterpanApplication.java
@@ -0,0 +1,13 @@
+package pl.com.peterpan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeterpanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeterpanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/com/peterpan/PeterpanApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.com.peterpan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeterpanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
